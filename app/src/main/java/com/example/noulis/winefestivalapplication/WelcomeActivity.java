69e3f46ab2a01ba78fc4c34bd712b58e50721b46
Konstantinos Noulis <konstantinos.noulis@nokia.com>
2017-06-17 19:39:49 +0300

    splash screen attempt

--- app/src/main/java/com/example/noulis/winefestivalapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/noulis/winefestivalapplication/WelcomeActivity.java
@@ -1,13 +1,52 @@
 package com.example.noulis.winefestivalapplication;
 
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ProgressBar;
 
 public class WelcomeActivity extends AppCompatActivity {
+    private ProgressBar progressBar;
+    int count = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
+        getSupportActionBar().hide();
+        progressBar = (ProgressBar) findViewById(R.id.progressBar2);
+        progressBar.setVisibility(View.VISIBLE);
+        progressBar.setMax(3);
+        progressBar.setProgress(0);
+        new proceedToMainActivityWithDelay().execute(3);
     }
+
+    class proceedToMainActivityWithDelay extends AsyncTask<Integer, Integer, String> {
+        @Override
+        protected String doInBackground(Integer... params) {
+            for (; count <= params[0]; count++) {
+                try {
+                    Thread.sleep(1000);
+                    publishProgress(count);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+        @Override
+        protected void onPostExecute(String result) {
+            getApplicationContext().startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        }
+        @Override
+        protected void onPreExecute() {
+        }
+        @Override
+        protected void onProgressUpdate(Integer... values) {
+            progressBar.setProgress(values[0]);
+        }
+    }
+
 }
