4fd5c11b7005bfbdfa98efd354d645877b559819
Konstantinos Noulis <konstantinos.noulis@nokia.com>
2017-06-18 13:00:02 +0300

    count down days for the fest - a bug persists in the calculation of the days - needs fix

--- app/src/main/java/com/example/noulis/winefestivalapplication/WelcomeActivity.java
+++ app/src/main/java/com/example/noulis/winefestivalapplication/WelcomeActivity.java
@@ -6,21 +6,44 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ProgressBar;
+import android.widget.TextView;
+
+import java.util.Calendar;
+import java.util.concurrent.TimeUnit;
 
 public class WelcomeActivity extends AppCompatActivity {
     private ProgressBar progressBar;
     int count = 1;
+    boolean beforeFestival;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         getSupportActionBar().hide();
+
+        // customize count down progress bar
         progressBar = (ProgressBar) findViewById(R.id.progressBar2);
         progressBar.setVisibility(View.VISIBLE);
-        progressBar.setMax(3);
+        progressBar.setMax(5);
         progressBar.setProgress(0);
-        new proceedToMainActivityWithDelay().execute(3);
+        new proceedToMainActivityWithDelay().execute(5);
+
+        // countdown days to the festival
+        TextView countDownMsg = (TextView) findViewById(R.id.daysTillFestival);
+        int daysTillFestival = getDaysTillFestival();
+        if (daysTillFestival == -1) {
+            beforeFestival = false;
+            countDownMsg.setText("Τέλος το κρασί για φέτος! Του χρόνου πάλι!");
+        }
+        else if (daysTillFestival == 0) {
+            beforeFestival = false;
+            countDownMsg.setText("Η Γιορτή έχει αρχίσει! Τρέξτε!");
+        }
+        else {
+            beforeFestival = true;
+            countDownMsg.setText(daysTillFestival + " μέρες ακόμα για τη Γιορτή Κρασιού!");
+        }
     }
 
     class proceedToMainActivityWithDelay extends AsyncTask<Integer, Integer, String> {
@@ -46,6 +69,34 @@ public class WelcomeActivity extends AppCompatActivity {
         @Override
         protected void onProgressUpdate(Integer... values) {
             progressBar.setProgress(values[0]);
+        }
+    }
+
+    private int getDaysTillFestival() {
+        // TODO: set actual date and time of the wine festival
+        Calendar wineFestivalStartDate = Calendar.getInstance();
+        //wineFestivalStartDate.set(Calendar.MINUTE, 0);
+        //wineFestivalStartDate.set(Calendar.HOUR, 21);
+        wineFestivalStartDate.set(Calendar.DAY_OF_MONTH, 18);
+        wineFestivalStartDate.set(Calendar.MONTH, Calendar.AUGUST);
+        wineFestivalStartDate.set(Calendar.YEAR, 2017);
+
+        Calendar wineFestivalEndDate = wineFestivalStartDate;
+        //wineFestivalEndDate.set(Calendar.HOUR, 2);
+        wineFestivalEndDate.set(Calendar.DAY_OF_MONTH, 21);
+
+        if (Calendar.getInstance().compareTo(wineFestivalStartDate) < 0) {
+            // before fest
+            // todo: have to return remaining hours in case app is run at the day of the fest
+            return wineFestivalStartDate.get(Calendar.DAY_OF_YEAR) - Calendar.getInstance().get(Calendar.DAY_OF_YEAR);
+        }
+        else if (Calendar.getInstance().compareTo(wineFestivalEndDate) < 0){
+            // during fest
+            return 0;
+        }
+        else {
+            // after fest
+            return -1;
         }
     }
 
