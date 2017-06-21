7ca9c7956529a0c6496b6c161dc5842f774a44e3
Konstantinos Noulis <konstantinos.noulis@nokia.com>
2017-06-21 18:02:46 +0300

    main activity additions

--- app/src/main/java/com/example/noulis/winefestivalapplication/MainActivity.java
+++ app/src/main/java/com/example/noulis/winefestivalapplication/MainActivity.java
@@ -1,5 +1,9 @@
 package com.example.noulis.winefestivalapplication;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,6 +14,9 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static String FACEBOOK_URL = "https://www.facebook.com/mixanitouxronou";
+    public static String FACEBOOK_PAGE_ID = "GOgVVVaeFgG";
+
     GridView gridView;
     static final String[] grid_options = new String[] {
             "Πρόγραμμα",
@@ -33,10 +40,59 @@ public class MainActivity extends AppCompatActivity {
 
         gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Toast.makeText(getApplicationContext(), ((TextView) v.findViewById(R.id.grid_item_label))
-                        .getText(), Toast.LENGTH_SHORT).show();
+                switch (((TextView)v.findViewById(R.id.grid_item_label)).getText().toString()) {
+                    case "Πρόγραμμα":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), ProgramActivity.class));
+                        break;
+                    case "Πώς να πας":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), MapActivity.class));
+                        break;
+                    case "Εισητήρια":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), TicketsActivity.class));
+                        break;
+                    case "Λαχειοφόρος Αγορά":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), LotteryActivity.class));
+                        break;
+                    case "Φωτογραφίες":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), PhotosActivity.class));
+                        break;
+                    case "Για τη γιορτή":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), AboutFestActivity.class));
+                        break;
+                    case "Για τη Ζίτσα":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), AboutZitsaActivity.class));
+                        break;
+                    case "Για τα οινοποιεία":
+                        //getApplicationContext().startActivity(new Intent(getApplicationContext(), AboutWineriesActivity.class));
+                        break;
+                    case "Facebook":
+                        Intent facebookIntent = new Intent(Intent.ACTION_VIEW);
+                        String facebookUrl = getFacebookPageURL(getApplicationContext());
+                        facebookIntent.setData(Uri.parse(facebookUrl));
+                        startActivity(facebookIntent);
+                        break;
+                    case "Twitter":
+                        Toast.makeText(getApplicationContext(), "Go to Twitter page", Toast.LENGTH_SHORT).show();
+                        break;
+                    default:
+                        Toast.makeText(getApplicationContext(), "Please select again", Toast.LENGTH_SHORT).show();
+                        break;
+                }
             }
         });
+    }
+
+    public String getFacebookPageURL(Context context) {
+        PackageManager packageManager = context.getPackageManager();
+        try {
+            int versionCode = packageManager.getPackageInfo("com.facebook.katana", 0).versionCode;
+            if (versionCode >= 3002850)
+                return "fb://facewebmodal/f?href=" + FACEBOOK_URL;
+            else
+                return "fb://page/" + FACEBOOK_PAGE_ID;
+        } catch (PackageManager.NameNotFoundException e) {
+            return FACEBOOK_URL;
+        }
     }
 
 }
